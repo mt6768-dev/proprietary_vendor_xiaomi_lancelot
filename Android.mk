c12c64755ed27865f900e95a885b51c77212d79e
Matsvei Niaverau <matvej2002@gmail.com>
2026-07-17 14:27:33 +0200

    lancelot: Initial vendor tree
    Change-Id: Iad76fc537fc08580d117c9239c381155a97df52d

---
+++ Android.mk
@@ -0,0 +1,18 @@
+#
+# Automatically generated file. DO NOT MODIFY
+#
+
+LOCAL_PATH := $(call my-dir)
+
+ifeq ($(TARGET_DEVICE),lancelot)
+
+$(call add-radio-file-sha1-checked,radio/lk.img,ee56ec5aaa9dfcd12ac4c252e879aeb67cc29bd8)
+$(call add-radio-file-sha1-checked,radio/logo.img,144e669eb7623e547d2273ca3cb56d24a5740474)
+$(call add-radio-file-sha1-checked,radio/md1img.img,df8c2e65dcdad0cd010a686d2912227f038d4abe)
+$(call add-radio-file-sha1-checked,radio/preloader_emmc.img,7ca32120d16f0d8ecad8f9ce85037efe5171138f)
+$(call add-radio-file-sha1-checked,radio/scp.img,cbba0a5fd66b67b30c6aa37393332fe0e654b9ab)
+$(call add-radio-file-sha1-checked,radio/spmfw.img,7ea319a432bbaf74a2064128ee28607667fe91f3)
+$(call add-radio-file-sha1-checked,radio/sspm.img,b9b19e168dff57b47ea108ee188226f3d61c380c)
+$(call add-radio-file-sha1-checked,radio/tee.img,6f2266bb89e72758e49d1858a0e6ad4a30ffb5f5)
+
+endif
